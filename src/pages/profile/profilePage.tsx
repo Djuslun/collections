0b87647c07d2e489e61d1b0f7f2544a545152f2f
Djuslun <djuslun@yandex.ru>
2024-01-01 13:54:44 +0300

    add user info section on profile page add user info skeleton on loading

--- src/pages/profile/profilePage.tsx
+++ src/pages/profile/profilePage.tsx
@@ -1,5 +1,25 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import { Navigate } from 'react-router-dom';
+import Container from 'ui/container';
+import UserInfo from './userInfo/userInfo';
+import './userInfo/userInfo.css';
+import UserInfoSkeleton from './userInfo/userInfoSkeleton';
+
 function ProfilePage() {
-    return <div>Profile page</div>;
+    const { user, isLoading, isAuthenticated } = useAuth0();
+
+    if ((!user || !isAuthenticated) && !isLoading) {
+        return <Navigate to="/" />;
+    }
+
+    return (
+        <Container>
+            <div className="pt-5">
+                {isLoading && <UserInfoSkeleton />}
+                {user && <UserInfo user={user} />}
+            </div>
+        </Container>
+    );
 }
 
 export default ProfilePage;
---
+++ src/pages/profile/userInfo/userInfoSkeleton.tsx
@@ -0,0 +1,30 @@
+import { Skeleton } from '@mui/material';
+
+function UserInfoSkeleton() {
+    return (
+        <div className="user-info-section">
+            <div className="user-image">
+                <Skeleton
+                    variant="rounded"
+                    sx={{
+                        height: '100%',
+                        width: '100%',
+                        background: 'rgba(0, 0, 0, 0.5)',
+                    }}
+                />
+            </div>
+            <div className="w-full smm:w-40">
+                <Skeleton
+                    variant="text"
+                    sx={{ fontSize: '1.85rem', background: 'rgba(0, 0, 0, 0.5)' }}
+                />
+                <Skeleton
+                    variant="text"
+                    sx={{ fontSize: '1.125rem', background: 'rgba(0, 0, 0, 0.5)' }}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default UserInfoSkeleton;
