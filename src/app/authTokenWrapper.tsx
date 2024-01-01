e1a8828b68d6be4e5ec904fe3565ad50f8ba41e2
Djuslun <djuslun@yandex.ru>
2024-01-01 15:56:33 +0300

    add auth token wrapper write auth token in user slice for prepare headers rtk quyry

---
+++ src/app/authTokenWrapper.tsx
@@ -0,0 +1,26 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import { useEffect, useState } from 'react';
+import { WithChidlren } from 'ts/interfaces';
+import LoaderWrapper from 'ui/loader/loaderWrapper';
+import { setAccsesToken } from '../redux/slices/userSlice';
+import { useAppDispath } from '../redux/useRedux';
+
+function AuthTokenWrapper({ children }: WithChidlren) {
+    const [isReady, setIsReady] = useState(false);
+    const dispatch = useAppDispath();
+    const { isAuthenticated, getAccessTokenSilently, isLoading } = useAuth0();
+    useEffect(() => {
+        if (isAuthenticated && !isLoading) {
+            getAccessTokenSilently()
+                .then((token) => dispatch(setAccsesToken(token)))
+                .then(() => setIsReady(true));
+        }
+        if (!isAuthenticated && !isLoading) {
+            setIsReady(true);
+        }
+    }, [isLoading]);
+
+    return <LoaderWrapper isLoading={!isReady}>{children}</LoaderWrapper>;
+}
+
+export default AuthTokenWrapper;
